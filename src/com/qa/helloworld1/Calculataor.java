61954aab1aa74849af26e1999f3b8cd0540a85a9
cruzgerman216 <cruzgerman216@yahoo.com>
2021-05-14 11:33:08 -0500

    gc: Calculator class

---
+++ src/com/qa/helloworld1/Calculataor.java
@@ -0,0 +1,24 @@
+package com.qa.helloworld1;
+
+public class Calculataor {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static double multiplication(double num1, double num2) {
+		return num1 * num2;
+	}
+
+	public static int subtraction(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public static int division(int num1, int num2) {
+		return num1 / num2;
+	}
+}
--- src/com/qa/helloworld1/Runner.java
+++ src/com/qa/helloworld1/Runner.java
@@ -56,4 +56,5 @@ public class Runner {
 	public static int add(int num1, int num2) {
 		return num1 + num2;
 	}
+
 }
