76d94407adc3e90b87be897f7bb5840e666fb429
cruzgerman216 <cruzgerman216@yahoo.com>
2021-05-14 09:41:47 -0500

    gc: QA Java Exercises Variable data types

---
+++ src/com/qa/helloworld1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.qa.helloworld1;
+
+public class HelloWorld {
+	 public static void main(String[] args) {
+	     printMessage("Hello World!");
+	 }
+	 public static void printMessage(String message) {
+	     System.out.println(message);
+	 }
+}
---
+++ src/com/qa/helloworld1/Runner.java
@@ -0,0 +1,28 @@
+package com.qa.helloworld1;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		//variables
+		// strings
+		// char, booleans, int, short, long, float, double, byte 
+		String myStr = "test";
+		int myInt = 1232;
+		char myChar = 'a';
+		short myShort = 1;
+		long myLong = 232342344;
+		float myFloat = 2.3f;
+		double myDouble = 0.4433;
+		byte myByte = 120;
+		System.out.println(myStr);
+		System.out.println(myInt);
+		System.out.println(myChar);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myByte);
+	}
+
+}
